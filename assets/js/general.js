42b42fb7334c1417e0cb70e6b523ff6de61e50d5
KhaledSaleh122 <voxagolde2003@gmail.com>
2024-10-11 20:51:53 +0300

    Add favourite bar

--- assets/js/general.js
+++ assets/js/general.js
@@ -2,3 +2,9 @@ const darkModeButton = document.getElementById("dark-mode");
 darkModeButton.addEventListener("click",function (event){
     document.body.classList.toggle('dark-mode');
 })
+
+const favouriteButton = document.getElementById("favourite-button");
+
+favouriteButton.addEventListener("click",function (event){
+    document.getElementById("favourite-bar").classList.toggle("hide");
+})
